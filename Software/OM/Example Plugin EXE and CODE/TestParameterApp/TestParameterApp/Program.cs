ad30a0ca37897893796754307b47c8d3dbae7978
tompbsmith@gmail.com <tompbsmith@gmail.com@7c8b6c0f-ac68-ff25-5287-5a457bef0f3f>
2012-12-21 19:32:55 +0000

    Added - Example plugin with EXE, HTML and XML in Plugins/ folder. There are comments in these files that tell you how to create your form in HTML and how to fill in the XML.
    Info - The example EXE/HTML/XML just takes the 3 strings it asks for and prints them to the console.
    
    TODO - Input files still might be buggy.

---
+++ Software/OM/Example Plugin EXE and CODE/TestParameterApp/TestParameterApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestParameterApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            foreach(string arg in args)
+            {
+                Console.WriteLine("arg" + i + ": " + arg);
+                i++;
+            }
+
+            string x = Console.ReadLine();
+        }
+    }
+}
