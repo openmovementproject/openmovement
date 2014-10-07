85361569e2942d2c4479af6366aa452e5878236f
openmovementnewcastle <danielgjackson@users.noreply.github.com>
2014-10-07 12:51:01 +0000

    OMGUI V20 - auto-updating - prompts for retry when setup and download log files can't be written.

--- Software/OM/omgui/AboutBox.Designer.cs
+++ Software/OM/omgui/AboutBox.Designer.cs
@@ -29,12 +29,12 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutBox));
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.labelCopyright = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.labelCompanyName = new System.Windows.Forms.Label();
-            this.labelCopyright = new System.Windows.Forms.Label();
             this.labelProductName = new System.Windows.Forms.Label();
-            this.labelVersion = new System.Windows.Forms.Label();
             this.labelDescription = new System.Windows.Forms.Label();
+            this.labelVersion = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -50,78 +50,87 @@
             this.tableLayoutPanel1.Controls.Add(this.labelProductName, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.labelDescription, 0, 5);
             this.tableLayoutPanel1.Controls.Add(this.labelVersion, 0, 4);
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(16, 15);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 6;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 72.63158F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 27.36842F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 59F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(317, 232);
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 33F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 73F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(423, 286);
             this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // labelCopyright
+            // 
+            this.labelCopyright.AutoSize = true;
+            this.labelCopyright.Location = new System.Drawing.Point(4, 117);
+            this.labelCopyright.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelCopyright.Name = "labelCopyright";
+            this.labelCopyright.Size = new System.Drawing.Size(46, 17);
+            this.labelCopyright.TabIndex = 2;
+            this.labelCopyright.Text = "label1";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(311, 63);
+            this.pictureBox1.Size = new System.Drawing.Size(415, 77);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // labelCompanyName
             // 
             this.labelCompanyName.AutoSize = true;
-            this.labelCompanyName.Location = new System.Drawing.Point(3, 69);
+            this.labelCompanyName.Location = new System.Drawing.Point(4, 85);
+            this.labelCompanyName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelCompanyName.Name = "labelCompanyName";
-            this.labelCompanyName.Size = new System.Drawing.Size(35, 13);
+            this.labelCompanyName.Size = new System.Drawing.Size(46, 17);
             this.labelCompanyName.TabIndex = 1;
             this.labelCompanyName.Text = "label1";
-            // 
-            // labelCopyright
-            // 
-            this.labelCopyright.AutoSize = true;
-            this.labelCopyright.Location = new System.Drawing.Point(3, 95);
-            this.labelCopyright.Name = "labelCopyright";
-            this.labelCopyright.Size = new System.Drawing.Size(35, 13);
-            this.labelCopyright.TabIndex = 2;
-            this.labelCopyright.Text = "label1";
             // 
             // labelProductName
             // 
             this.labelProductName.AutoSize = true;
-            this.labelProductName.Location = new System.Drawing.Point(3, 119);
+            this.labelProductName.Location = new System.Drawing.Point(4, 147);
+            this.labelProductName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelProductName.Name = "labelProductName";
-            this.labelProductName.Size = new System.Drawing.Size(35, 13);
+            this.labelProductName.Size = new System.Drawing.Size(46, 17);
             this.labelProductName.TabIndex = 3;
             this.labelProductName.Text = "label1";
-            // 
-            // labelVersion
-            // 
-            this.labelVersion.AutoSize = true;
-            this.labelVersion.Location = new System.Drawing.Point(3, 146);
-            this.labelVersion.Name = "labelVersion";
-            this.labelVersion.Size = new System.Drawing.Size(35, 13);
-            this.labelVersion.TabIndex = 4;
-            this.labelVersion.Text = "label1";
             // 
             // labelDescription
             // 
             this.labelDescription.AutoSize = true;
-            this.labelDescription.Location = new System.Drawing.Point(3, 172);
+            this.labelDescription.Location = new System.Drawing.Point(4, 212);
+            this.labelDescription.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.labelDescription.Name = "labelDescription";
-            this.labelDescription.Size = new System.Drawing.Size(35, 13);
+            this.labelDescription.Size = new System.Drawing.Size(46, 17);
             this.labelDescription.TabIndex = 5;
             this.labelDescription.Text = "label1";
             // 
+            // labelVersion
+            // 
+            this.labelVersion.AutoSize = true;
+            this.labelVersion.Location = new System.Drawing.Point(4, 180);
+            this.labelVersion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(46, 17);
+            this.labelVersion.TabIndex = 4;
+            this.labelVersion.Text = "label1";
+            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(129, 250);
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Location = new System.Drawing.Point(172, 308);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 1;
             this.button1.Text = "OK";
             this.button1.UseVisualStyleBackColor = true;
@@ -129,16 +138,19 @@
             // 
             // AboutBox
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(341, 283);
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(455, 348);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.tableLayoutPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutBox";
-            this.Padding = new System.Windows.Forms.Padding(9);
+            this.Padding = new System.Windows.Forms.Padding(12, 11, 12, 11);
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
--- Software/OM/omgui/Program.cs
+++ Software/OM/omgui/Program.cs
@@ -35,6 +35,7 @@ namespace OmGui
                 string logFile = Path.GetDirectoryName(Application.ExecutablePath) + Path.DirectorySeparatorChar + "log.txt";
                 string configDumpFile = null; // Path.GetDirectoryName(Application.ExecutablePath) + Path.DirectorySeparatorChar + "dump.csv";
                 string downloadDumpFile = null;
+                bool noUpdateCheck = false;
 
                 int positional = 0;
                 int uac = 0;
@@ -43,6 +44,7 @@ namespace OmGui
                     if (args[i].ToLower() == "-log") { logFile = args[++i]; }
                     else if (args[i].ToLower() == "-configlog" || args[i].ToLower() == "-dump") { configDumpFile = args[++i]; }
                     else if (args[i].ToLower() == "-downloadlog") { downloadDumpFile = args[++i]; }
+                    else if (args[i].ToLower() == "-noupdatecheck") { noUpdateCheck = true; }
                     else if (args[i].ToLower() == "-uac:none") { uac = 0; }
                     else if (args[i].ToLower() == "-uac:ask")     { uac = 1; }
                     else if (args[i].ToLower() == "-uac:always")  { uac = 2; }
@@ -66,7 +68,7 @@ namespace OmGui
                     }
                 }
 
-                MainForm mainForm = new MainForm(uac, configDumpFile, downloadDumpFile);
+                MainForm mainForm = new MainForm(uac, configDumpFile, downloadDumpFile, noUpdateCheck);
                 //mainForm.LogFile = logFile;
 
                 Application.Run(mainForm);
