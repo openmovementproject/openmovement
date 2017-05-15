26923d17ca6d281fba994c084067f0ad736457ba
Dan Jackson <email@danieljackson.co.uk>
2017-05-15 11:58:40 +0100

    omconvert: fix for +/-16g WAV output.

--- Software/OM/omapi/tests/locktest/build.cmd
+++ Software/OM/omapi/tests/locktest/build.cmd
@@ -2,9 +2,17 @@
 SETLOCAL
 CD /D %~dp0
 
+IF EXIST "%VS71COMNTOOLS%\vsvars32.bat"  SET COMNTOOLS=%VS71COMNTOOLS%
+IF EXIST "%VS80COMNTOOLS%\vsvars32.bat"  SET COMNTOOLS=%VS80COMNTOOLS%
+IF EXIST "%VS90COMNTOOLS%\vsvars32.bat"  SET COMNTOOLS=%VS90COMNTOOLS%
 IF EXIST "%VS100COMNTOOLS%\vsvars32.bat" SET COMNTOOLS=%VS100COMNTOOLS%
+IF EXIST "%VS110COMNTOOLS%\vsvars32.bat" SET COMNTOOLS=%VS110COMNTOOLS%
+IF EXIST "%VS120COMNTOOLS%\vsvars32.bat" SET COMNTOOLS=%VS120COMNTOOLS%
+IF EXIST "%VS130COMNTOOLS%\vsvars32.bat" SET COMNTOOLS=%VS130COMNTOOLS%
+IF EXIST "%VS140COMNTOOLS%\vsvars32.bat" SET COMNTOOLS=%VS140COMNTOOLS%
+
 IF NOT "%COMNTOOLS%"=="" GOTO C_COMPILER
-ECHO ERROR: No known 'VS##COMNTOOLS' environment variable set - couldn't find vsvars32.bat for C compiler.
+ECHO ERROR: No known 'VS###COMNTOOLS' environment variable set - couldn't find vsvars32.bat for C compiler.
 GOTO END
 
 :C_COMPILER
@@ -19,13 +27,7 @@ ECHO Linking...
 link locktest /defaultlib:user32.lib DeviceFinder omapi-devicefinder omapi-download omapi-internal omapi-main omapi-reader omapi-settings omapi-status /out:locktest.exe
 IF ERRORLEVEL 1 GOTO ERROR
 
-rem GOTO END
-
-:RUN_TEST
-ECHO Running test program...
-locktest
 GOTO END
-
 
 :ERROR
 ECHO ERROR: An error occured.
