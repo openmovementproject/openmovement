e5cbaf0ae468243329ac9327fe02b0239f21a561
Daniel Jackson <email@danieljackson.co.uk>
2019-11-18 15:45:15 +0000

    Border, drop-shadow.

--- Software/OM/omgui/Splash.cs
+++ Software/OM/omgui/Splash.cs
@@ -19,6 +19,23 @@ namespace OmGui
             InitializeComponent();
         }
 
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                const int CS_DROPSHADOW = 0x20000;
+                CreateParams createParams = base.CreateParams;
+                createParams.ClassStyle |= CS_DROPSHADOW;
+                return createParams;
+            }
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.DrawRectangle(Pens.Gray, new Rectangle(0, 0, Width - 1, Height - 1));
+        }
+
         private void Splash_Load(object sender, EventArgs e)
         {
             //labelStatus.Text = "Loading...";
