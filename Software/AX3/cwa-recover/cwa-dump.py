9b00225c615f1d1529fff776ece82f7253a80d06
Daniel Jackson <email@danieljackson.co.uk>
2023-11-28 15:21:20 +0000

    Allow resume/overwrite.

--- Software/AX3/cwa-recover/cwa-dump.py
+++ Software/AX3/cwa-recover/cwa-dump.py
@@ -8,24 +8,29 @@ from datetime import timedelta
 import ctypes
 import sys
 import os
+import atexit
 #import io
 
-def driveDump(path, outputFile):
+def driveDump(path, outputFile, mode):
   blockSize = 128 * 1024
   
   print("DriveDump:", path)
   try:
+    # AX3 drive size: fileSize = 992161*512
+    # AX6 drive size: fileSize = 1975995*512
+    print("Detecting device physical drive size...")
+    fileSize = findPhysicalDriveSize(path)
+    if fileSize <= 0:
+      print("ERROR: Problem determining drive size.")
+      return
+
     startTime = time.time()
     with open(path, 'rb') as fi:
-      # AX3 drive size: fileSize = 992161*512
-      # AX6 drive size: fileSize = 1975995*512
-      fileSize = findPhysicalDriveSize(path)
-      if fileSize <= 0:
-        print("ERROR: Problem determining drive size.")
-        return
-      
-      with open(outputFile, 'wb') as fo:
+      with open(outputFile, mode) as fo:
         writtenSize = 0
+        # Resume
+        offset = fo.tell()
+        fi.seek(offset)
         while True:
           data = fi.read(blockSize)
           size = len(data)
@@ -37,19 +42,23 @@ def driveDump(path, outputFile):
             print("ERROR: Problem writing all of the data, wrote " + str(written) + " of " + str(size) + "")
             break
             
+          offset += size
           writtenSize += written
-          perc = round(100 * writtenSize / fileSize, 1)
+          perc = round(100 * offset / fileSize, 1)
           elapsed = time.time() - startTime
           remaining = 0
           if elapsed > 0:
             rate = writtenSize / elapsed
             if rate > 0:
-              remaining = (fileSize - writtenSize) / rate
-          print("Dumping " + str(written) + ", =" + str(writtenSize) + "/" + str(fileSize) + " (" + str(perc) + "%) in " + str(timedelta(seconds=int(elapsed))) + ", " + str(round(rate / 1024, 3)) + " kB/s, ETA " + str(timedelta(seconds=int(remaining))) + ".")
+              remaining = (fileSize - offset) / rate
+          print("Dumping " + str(written) + " =" + str(writtenSize) + " @" + str(offset) + " /" + str(fileSize) + " (" + str(perc) + "%) in " + str(timedelta(seconds=int(elapsed))) + ", " + str(round(rate / 1024, 3)) + " kB/s, ETA " + str(timedelta(seconds=int(remaining))) + ".")
           
-          if writtenSize >= fileSize:
+          if offset >= fileSize:
             break
-        
+
+  except FileExistsError:
+    print("ERROR: Output file already exists.  Remove, rename, or use options --overwrite or --resume: ", outputFile)
+
   except PermissionError:
     print("ERROR: Permission error -- you must run this in an Ctrl+Shift+Esc, Alt+F, N, cmd, 'Create this task with administrative privileges.'")
     return
@@ -106,22 +115,48 @@ def is_admin():
     return False
 
 
+def pause():
+  print("Press Enter to continue...")
+  input()
+
 def main():
+  print("Checking admin permissions...")
+  if not is_admin():
+    # Re-run the program with admin rights
+    params = " ".join(['"%s"' % (x,) for x in sys.argv[0:]])
+    print("Spawning version with admin rights...", sys.executable, params)
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, params, None, 1)
+    return
+
   print("Running...")
-  outputFile = "cwa-dump.img"
-  if len(sys.argv) > 1:
-    outputFile = sys.argv[1]
-    print("NOTE: Using output file:", outputFile)
-    
-  if len(sys.argv) > 2:
-    print("ERROR: Too many parameters passed")
-    return
+  atexit.register(pause);
+
+  outputFile = None
+  mode = "xb"
+  arg = 1
+  while arg < len(sys.argv):
+    if sys.argv[arg].startswith("-"):
+      if sys.argv[arg] == "--no-overwrite":
+        mode = "xb"
+      elif sys.argv[arg] == "--overwrite":
+        mode = "wb"
+      elif sys.argv[arg] == "--resume":
+        mode = "ab"
+      else:
+        print("ERROR: Unrecognized option: " + sys.argv[arg])
+        return
+    elif outputFile == None:
+      outputFile = sys.argv[arg]
+    else:
+      print("ERROR: Unrecognized positional argument: " + sys.argv[arg])
+      return
+    arg += 1
+
+  if outputFile is None:
+    outputFile = "cwa-dump.img"
+  print("NOTE: Using output file in mode=" + mode + ":", outputFile)
   
-  if os.path.exists(outputFile):
-    print("ERROR: Output file already exists, must remove or use another output file:", outputFile)
-    return
-  
-  print("Autodetect...")
+  print("Detecting device physical drive...")
   prefixDevice = [b"AX3 AX3 Mass Storage USB Device", b"AX6 AX6 Mass Storage USB Device"]
   prefixDrive = b"\\\\.\\PHYSICALDRIVE"
   drivePath = findSingleDrive(prefixDevice, prefixDrive)
@@ -129,15 +164,8 @@ def main():
     print("ERROR: Cannot continue without a device drive.")
     return
 
-  print("Checking admin permissions...")
-  if is_admin():
-    # Run code needing admin rights
-    driveDump(drivePath, outputFile)
-  else:
-    # Re-run the program with admin rights
-    params = " ".join(['"%s"' % (x,) for x in sys.argv[0:]])
-    print("Spawning version with admin rights...", sys.executable, params)
-    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, params, None, 1)
+  # Run code needing admin rights
+  driveDump(drivePath, outputFile, mode)
 
 if __name__ == "__main__":
   main()
